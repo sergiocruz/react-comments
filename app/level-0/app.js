b30773db4226b8369acd8781994e87c6779809e8
Sergio Cruz <hello@sergiocruz.me>
2015-08-26 20:17:56 -0600

    Level 0: more ideas to explain JSX

--- app/level-0/app.js
+++ app/level-0/app.js
@@ -1,5 +1,5 @@
 /**
-  Level 0 App:
+  #Level 0 App
 
   In this phase of our app, we're returning the equivalent of
   static html. Although this syntax looks a lot like html, it's
@@ -9,7 +9,20 @@
   "html looking code", the resulting code looks something
   like this:
 
+  ```js
   return React.createElement("div", { className: "comments-app" }, ...);
+  ```
+
+  **JSX gotchas:**
+  1. Our render method must return exacly one top level JSX element.
+  1.1 If more elements are needed, wrap all elements in a `<div>` tag or similar.
+
+  2. JavaScript reserved words are not allowed for attributes: ie. `class`, `for`, etc.
+  2.1 JSX offers alternatives for these, such as `className`, `htmlFor`, etc.
+
+  **Possible ideas to explain JSX:**
+  1. Compare JSX with CSS prepocessors that need a build step between
+  editing code and browser reading code.
 
  */
 
